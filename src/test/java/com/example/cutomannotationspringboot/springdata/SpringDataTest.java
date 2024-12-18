9dada3f50d31400e82a2fc151ed9ea139f430e25
tamphung397 <tamphung397@gmail.com>
2024-12-19 01:16:47 +0700

    update

--- src/test/java/com/example/cutomannotationspringboot/springdata/SpringDataTest.java
+++ src/test/java/com/example/cutomannotationspringboot/springdata/SpringDataTest.java
@@ -1,26 +1,30 @@
-//package com.example.cutomannotationspringboot.springdata;
-//
-//import com.example.cutomannotationspringboot.dto.staff.CreateStaffDto;
-//import com.example.cutomannotationspringboot.service.StaffService;
-//import com.example.cutomannotationspringboot.springdata.entity.Staff;
-//import com.example.cutomannotationspringboot.springdata.entity.User;
-//import com.example.cutomannotationspringboot.springdata.repository.StaffRepository;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.test.context.junit4.SpringRunner;
-//
+package com.example.cutomannotationspringboot.springdata;
+
+import com.example.cutomannotationspringboot.dto.staff.CreateStaffDto;
+import com.example.cutomannotationspringboot.service.StaffService;
+import com.example.cutomannotationspringboot.springdata.entity.Staff;
+import com.example.cutomannotationspringboot.springdata.entity.User;
+import com.example.cutomannotationspringboot.springdata.repository.StaffRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 //@SpringBootTest
 //@RunWith(SpringRunner.class)
-//public class SpringDataTest {
-//    @Autowired
-//    private StaffService staffService;
-//
-//    @Test
-//    public void createStaff(){
-//        CreateStaffDto createStaffDto = new CreateStaffDto("staff1","Dev",4000,"test2","1234","test@gmail.com");
-//        staffService.createStaff(createStaffDto);
-//    }
-//}
+public class SpringDataTest {
+    @Autowired
+    private StaffService staffService;
+
+    @Test
+    public void createStaff() throws IOException {
+//        System.out.println("oke123");
+//        Files.createDirectory(Paths.get("/tmp") );
+        Files.createTempFile(Paths.get("/tmp"), null, null);
+    }
+}
---
+++ src/test/java/com/example/cutomannotationspringboot/validator/ValidatorTestProductTest.java
@@ -0,0 +1,59 @@
+//package com.example.cutomannotationspringboot.validator;
+//
+//
+//import com.example.cutomannotationspringboot.dto.product.CreateProductDto;
+//import com.example.cutomannotationspringboot.exception.NotInRangeException;
+//import com.example.cutomannotationspringboot.exception.NotNullException;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.TestConfiguration;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//@RunWith(SpringRunner.class)
+//public class ValidatorTestProductTest {
+//
+//    @TestConfiguration
+//    static class ValidationTestProductConfiguration{
+//        @Bean
+//        RequestValidator validator(){
+//            return new RequestValidator();
+//        }
+//
+//        @Bean
+//        TamRangeValidator tamRangeValidator(){
+//            return new TamRangeValidator();
+//        }
+//
+//        @Bean
+//        TamLengthValidator tamLengthValidator(){
+//            return new TamLengthValidator();
+//        }
+//
+//        @Bean
+//        NotNullValidator notNullValidator(){
+//            return new NotNullValidator();
+//        }
+//    }
+//
+//    @Autowired
+//    RequestValidator validator;
+//    @Test
+//    public void testValid() throws Exception {
+//        CreateProductDto productDto = new CreateProductDto("Iphone", 4000, "Phone");
+//        validator.validate(productDto);
+//    }
+//
+//    @Test(expected = NotInRangeException.class)
+//    public void testInValidRangePrice() throws Exception {
+//        CreateProductDto productDto = new CreateProductDto("Iphone", 1000, "Phone");
+//        validator.validate(productDto);
+//    }
+//
+//    @Test(expected = NotNullException.class)
+//    public void testInValidNotNullName() throws Exception {
+//        CreateProductDto productDto = new CreateProductDto(null, 4000, "SmartPhone");
+//        validator.validate(productDto);
+//    }
+//}
